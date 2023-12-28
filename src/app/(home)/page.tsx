9c1c08b1eb6900b937417e1d1da7e8f924e979c3
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-12-28 00:39:44 -0300

    refactor: implement posts from content layer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,58 +1,32 @@
 import { renderCoffee } from '@/utils/renderCoffee'
 import { timeNeededToReadInMinutes } from '@/utils/timeNeededToReadInMinutes'
+import { allPosts } from 'contentlayer/generated'
 import dayjs from 'dayjs'
 import Link from 'next/link'
-import { z } from 'zod'
-
-const postsSchema = z
-	.object({
-		id: z.number(),
-		title: z.string(),
-		body: z.string(),
-		number: z.number(),
-		created_at: z.string(),
-	})
-	.array()
-
-const headers = new Headers({
-	Authorization: `Bearer ${process.env.GITHUB_TOKEN || ''}`,
-})
 
 export default async function HomePage() {
-	const responsePosts = await fetch(
-		'https://api.github.com/repos/davyd-souza/dicebugging/issues',
-		{
-			method: 'GET',
-			headers,
-			next: {
-				revalidate: 60 * 10, // 10 minutes
-			},
-		},
-	).then((res) => res.json())
-
-	const posts = postsSchema.parse(responsePosts)
+  const posts = allPosts
 
 	return (
-		<main className="max-w-3xl mx-auto px-2 md:px-0">
-			{posts.map(({ body, created_at, id, title, number }) => (
-				<article key={id} className="grid gap-2">
+			{posts.map(({ _id, _raw, body, description, created_at, title, slug }) => (
+				<article key={_id} className="grid gap-2">
 					<header>
 						<h2 className="text-xl text-primary font-bold">{title}</h2>
 
 						<div className="text-xs inline-flex gap-1 text-neutral-400">
 							<p>{dayjs(created_at).format('LL').toLowerCase()}</p>
 
-							<span>•</span>
+							<span>&bull;</span>
 
-							<p>{`${renderCoffee(body.length)} ${timeNeededToReadInMinutes(
-								body.length,
+							<p>{`${renderCoffee(body.raw.length)} ${timeNeededToReadInMinutes(
+								body.raw.length,
 							)}`}</p>
 						</div>
 					</header>
 
-					<p className="line-clamp-2">{body.slice(0, 300)}</p>
+					<p className="line-clamp-2">{description}</p>
 
-					<Link href={`/post/${number}`} className="font-bold">
+					<Link href={slug} className="font-bold">
 						Read more
 					</Link>
 				</article>
